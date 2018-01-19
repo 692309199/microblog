d243b0b61e5cbceec4e8b6876299e0042eb5134e
owen <692309199@qq.com>
2018-01-19 02:00:33 -0800

    add if to template/index.html

--- app/views.py
+++ app/views.py
@@ -1,6 +1,19 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world!"
+    user = { 'nickname': 'owen'}
+    return render_template("index.html",
+           title = 'Home',
+           user = user)
+#<html>
+#  <head>
+#   <title>Home Page</title>
+#  </head>
+#  <body>
+#    <h1>hello, ''' + user['nickname'] + '''</h1>
+#  </body>
+#<html>
+#'''
